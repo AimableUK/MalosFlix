0cb126bbc40dc66386b15e86fc255b40946b1968
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-03-14 23:06:28 +0200

    Added codes for 404 Page

--- src/components/404Page/Page404.jsx
+++ src/components/404Page/Page404.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import page404gif from '../../img/page404.gif'
+
+
 
 const Page404 = () => {
   return (
-    <div>
-      <p>Page Not found</p>
+    <div className='flex flex-col items-center'>
+      <p className='text-red-400 text-9xl'>404</p>
+      <p className='font-homenaje text-3xl'>PAGE NOT FOUND</p>
+      <img src={page404gif} />
     </div>
   )
 }
