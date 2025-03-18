d7770b647df271ae504c31b03af9f62b6aecc1f6
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-03-18 10:13:05 +0200

    handled erros with my landingbg

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import LandingBg from "./assets/LandingBg.jpg";
+import LandingBg from './assets/LandingBg.jpg';
 import PageTitleUpdater from "./components/TitleUpdater/PageTitleUpdater";
 
 const Layout = () => {
