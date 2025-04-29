12422e4f72d9adde3c21a42b56a4ad8e5c7bd807
UKOBIZABA Aimable <154277022+AimableTrague@users.noreply.github.com>
2025-04-29 10:56:23 +0200

    added analytics and react router dom

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,8 +3,8 @@ import { Outlet, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import PageTitleUpdater from "./components/TitleUpdater/PageTitleUpdater";
-import LandingBg from "./assets/landingBg.jpg"
-// import { Analytics } from "@vercel/analytics/react"
+import LandingBg from "./assets/landingBg.jpg";
+import { Analytics } from "@vercel/analytics/react";
 
 const Layout = () => {
   const location = useLocation();
@@ -13,19 +13,24 @@ const Layout = () => {
   return (
     <div
       className={`font-body text-gray-200 min-h-screen ${
-        isHome
-          ? ""
-          : "bg-gradient-to-b from-black to-gray-900"
+        isHome ? "" : "bg-gradient-to-b from-black to-gray-900"
       }`}
-      style={isHome ? { backgroundImage: `url(${LandingBg})`, backgroundSize: "cover", backgroundPosition: "center", height:'100vh'} : {}}
+      style={
+        isHome
+          ? {
+              backgroundImage: `url(${LandingBg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "100vh",
+            }
+          : {}
+      }
     >
-      {isHome && (
-        <div className="absolute inset-0 bg-black opacity-50"></div>
-      )}
+      {isHome && <div className="absolute inset-0 bg-black opacity-50"></div>}
 
       <div className="relative z-10 text-white">
         <PageTitleUpdater />
-        {/* <Analytics /> */}
+        <Analytics />
         <Header />
         <Outlet />
         <Footer />
